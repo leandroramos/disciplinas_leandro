96fe6f6a71cb8057f7e083cee05909fe21bfc5d3
Leandro Ramos <leandroramos@usp.br>
2018-03-10 22:33:21 -0300

    Método destroy funcionando

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,4 @@ Route::get('/disciplinas/{disciplina}','DisciplinaController@show');
 Route::post('/disciplinas', 'DisciplinaController@store');
 Route::get('/disciplinas/{disciplina}/edit','DisciplinaController@edit');
 Route::patch('/disciplinas/{disciplina}','DisciplinaController@update');
+Route::delete('/disciplinas/{disciplina}','DisciplinaController@destroy');
